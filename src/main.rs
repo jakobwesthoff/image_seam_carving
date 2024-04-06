a2a7d4558b46d6784dc365bfc0805540e61dd1f7
Jakob Westhoff <jakob@westhoffswelt.de>
2024-04-06 22:10:10 +0200

    Prepared basic image input/output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+
+struct Image {
+    width: u32,
+    height: u32,
+    pixels: Vec<u8>,
+}
+
+impl Image {
+    fn save(&self, path: &str) -> anyhow::Result<()> {
+        let image = image::RgbaImage::from_raw(self.width, self.height, self.pixels.clone())
+            .context("create image")?;
+
+        image.save(path).context("save image to file")?;
+
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let image =
+        image::open("./assets/Broadway_tower_edit.jpg").context("load original image")?;
+
+    let image = Image {
+        width: image.width(),
+        height: image.height(),
+        pixels: image.to_rgba8().into_vec(),
+    };
+
+    image.save("./output.png").context("save image")?;
+
+    Ok(())
 }
