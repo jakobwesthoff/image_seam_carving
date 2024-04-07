4c785cf6ed17e21f090aa24ba1088d17b83003e2
Jakob Westhoff <jakob@westhoffswelt.de>
2024-04-07 02:11:26 +0200

    Implement seam carving from image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 
-type Pixel = (f64, f64, f64, f64);
+type Pixel = Option<(f64, f64, f64, f64)>;
 
 #[derive(Clone)]
 struct Matrix<T>
@@ -31,7 +31,9 @@ impl Matrix<Pixel> {
             self.height,
             self.data
                 .iter()
-                .flat_map(|(r, g, b, a)| {
+                .filter(|pixel| pixel.is_some())
+                .flat_map(|pixel| {
+                    let (r, g, b, a) = pixel.unwrap();
                     vec![
                         (r * 255.0).round().clamp(0.0, 255.0) as u8,
                         (g * 255.0).round().clamp(0.0, 255.0) as u8,
@@ -46,6 +48,28 @@ impl Matrix<Pixel> {
         image.save(path).context("save image to file")?;
 
         Ok(())
+    }
+
+    fn remove_seam(&mut self, seam: &Seam) {
+        'iterate_rows: for y in 0..self.height {
+            // Find x by scanning the row, as we are storing the image with Optional values.
+            // This is not the most intelligent method and more or less a hack,
+            // but was the quickest and dirtiest way to make it work I could
+            // think of in the middle of the night :).
+            let mut index = 0;
+            let stride = self.data.len() / self.height as usize;
+            for x in 0..stride {
+                if self.data[y as usize * stride + x].is_some() {
+                    if index == seam[y as usize] {
+                        self.data[y as usize * stride + x as usize] = None;
+                        continue 'iterate_rows;
+                    } else {
+                        index += 1;
+                    }
+                }
+            }
+        }
+        self.width = self.width - 1;
     }
 }
 
@@ -134,12 +158,12 @@ fn load_image(path: &str) -> anyhow::Result<ImageMatrix> {
             .into_vec()
             .chunks(4)
             .map(|pixel| {
-                (
+                Some((
                     pixel[0] as f64 / 255.0,
                     pixel[1] as f64 / 255.0,
                     pixel[2] as f64 / 255.0,
                     pixel[3] as f64 / 255.0,
-                )
+                ))
             })
             .collect(),
     })
@@ -152,7 +176,11 @@ fn calculate_luminance(image: &ImageMatrix) -> LuminanceMatrix {
         data: image
             .data
             .iter()
-            .map(|(r, g, b, _a)| 0.2126 * r + 0.7152 * g + 0.0722 * b)
+            .filter(|pixel| pixel.is_some())
+            .map(|pixel| {
+                let (r, g, b, _) = pixel.unwrap();
+                0.2126 * r + 0.7152 * g + 0.0722 * b
+            })
             // .map(|(r, g, b, _a)| 0.299 * r + 0.587 * g + 0.114 * b)
             // .map(|(r, g, b, _a)| (0.299 * r.powi(2) + 0.587 * g.powi(2) + 0.114 * b.powi(2)).sqrt())
             .collect(),
@@ -261,7 +289,7 @@ fn calculate_seam(dp: &DpMatrix) -> Seam {
     let mut seam = vec![0; dp.height as usize];
     seam[dp.height as usize - 1] = min_x_index;
 
-    for y in (0..=dp.height -2).rev() {
+    for y in (0..=dp.height - 2).rev() {
         min_x = f64::MAX;
         let mut next_min_x_index = min_x_index;
         for dx in -1..=1 as isize {
@@ -283,35 +311,42 @@ fn calculate_seam(dp: &DpMatrix) -> Seam {
 }
 
 fn main() -> anyhow::Result<()> {
-    let image = load_image("./assets/Broadway_tower_edit.jpg")?;
+    let mut image = load_image("./assets/Broadway_tower_edit.jpg")?;
 
-    // Calculate luminance of the image
-    // https://stackoverflow.com/a/596243
-    let luminance = calculate_luminance(&image);
-    luminance.print_min_max("luminance");
-    luminance
-        .save("./luminance.png")
-        .context("save luminance")?;
+    for i in 0..510 {
+        println!("Calculate and remove vertical seam: {}", i);
+        // Calculate luminance of the image
+        // https://stackoverflow.com/a/596243
+        let luminance = calculate_luminance(&image);
+        // luminance.print_min_max("luminance");
+        // luminance
+        //     .save("./luminance.png")
+        //     .context("save luminance")?;
 
-    // Apply sobel operator to get luminance gradient
-    let gradient = calculate_gradient(&luminance);
-    gradient.print_min_max("gradient");
-    gradient.save("./gradient.png").context("save gradient")?;
+        // Apply sobel operator to get luminance gradient
+        let gradient = calculate_gradient(&luminance);
+        // gradient.print_min_max("gradient");
+        // gradient.save("./gradient.png").context("save gradient")?;
 
-    // Use Dynamic Programming to calculate the prerequisite to find the path
-    // with the lowest energy.
-    let dp = calculate_dp(&gradient);
-    dp.print_min_max("dp");
-    dp.save("dp.png")?;
+        // Use Dynamic Programming to calculate the prerequisite to find the path
+        // with the lowest energy.
+        let dp = calculate_dp(&gradient);
+        // dp.print_min_max("dp");
+        // dp.save("dp.png")?;
 
-    let seam = calculate_seam(&dp);
+        let seam = calculate_seam(&dp);
 
-    let mut new_image = image.clone();
-    for y in 0..seam.len() {
-        new_image.set(y, seam[y] as usize, (255.0, 255.0, 0.0, 255.0));
+        // let mut new_image = image.clone();
+        // for y in 0..seam.len() {
+        //     new_image.set(y, seam[y] as usize, (255.0, 255.0, 0.0, 255.0));
+        // }
+
+        // new_image.save("./seam.png")?;
+
+        image.remove_seam(&seam);
     }
 
-    new_image.save("./seam.png")?;
+    image.save("./resized.png")?;
 
     Ok(())
 }
